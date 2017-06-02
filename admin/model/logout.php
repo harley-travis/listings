d9fbdbe0ed3fa510e757e8d9bf2ea438404f0063
harley-travis <harley.travis2@gmail.com>
2017-06-02 13:38:15 -0600

    removed admin folder.

--- admin/model/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// log the user out of the session
-session_start();
-unset($_SESSION['username']); // remove this session variable
-header("Location: ../index.php"); // redirct those suckers back to the home page
-
-
-?>
